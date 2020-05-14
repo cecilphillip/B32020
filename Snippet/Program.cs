01fcdd471e11e080359ac9180edcf684a20f9fa1
cecilphillip <cecilphillip@gmail.com>
2020-05-14 18:38:53 -0400

    adding snippet

---
+++ Snippet/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Snippet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string secret = "uggcf://nxn.zf/ohvyqgunaxf\nGunax*Lbh*Nyy!*🌮🌮";
+            string result = string.Empty;
+            
+            foreach (char c in secret)
+            {
+                if((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+                { int cc = ((c & 223) - 52) % 26 + (c & 32) + 65; result+=(char)cc; }
+                else if (c == '*') { result+=" "; } 
+                else { result += c; }
+            }
+            System.Console.WriteLine(result.ToString());
+        }
+    }
+}
